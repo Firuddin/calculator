1e6bcd31ea8fd3cfc6610cb2750b5da444ab25a4
Firuddin <Firuddinaliyev888@gmail.com>
2024-04-09 00:02:12 -0700

    add js

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ let result = document.getElementById("result")
 function getResult(value){
     if (value==="=") {
         result.value =eval(result.value)
+    }else{
+        result.value+=value
     }
+}
+
+function clearResult(){
+    result.value = ""
 }
